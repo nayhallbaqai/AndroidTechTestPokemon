c2199a9f2ffbeaa8a5058bddfa0a01e9ce5301da
nayhallbaqai <nayhallbaqai@hotmail.com>
2020-02-27 23:31:18 +0000

    NB: Implemented step 2 & 3 of the game, used Kotlin to build RoundFactoryImpl as well as written some tests in RoundFactoryImplTest.

--- app/src/main/java/io/deepmatter/pokemon/core/RoundFactory.kt
+++ app/src/main/java/io/deepmatter/pokemon/core/RoundFactory.kt
@@ -6,4 +6,8 @@ import io.deepmatter.pokemon.viewmodel.Round
 interface RoundFactory {
 
     fun buildRound(cards: List<Card>): Round
+
+    fun getRarestCard(cardOne: Card, cardTwo: Card): Card
+
+    fun compareTo(cardOne: Card, cardTwo: Card): Int
 }
--- app/src/main/java/io/deepmatter/pokemon/core/RoundFactoryImpl.kt
+++ app/src/main/java/io/deepmatter/pokemon/core/RoundFactoryImpl.kt
@@ -1,10 +1,62 @@
 package io.deepmatter.pokemon.core
 
 import io.deepmatter.pokemon.model.Card
+import io.deepmatter.pokemon.model.Rarity
 import io.deepmatter.pokemon.util.random.Randomiser
 import io.deepmatter.pokemon.viewmodel.Round
 
 class RoundFactoryImpl(private val randomiser: Randomiser) : RoundFactory {
 
-    override fun buildRound(cards: List<Card>): Round = Round()
+    private val mapRarities: Map<Rarity, Int> = mapOf(
+        Rarity.Common to 1,
+        Rarity.Uncommon to 2,
+        Rarity.Rare to 3,
+        Rarity.RareHolo to 4,
+        Rarity.RareUltra to 5,
+        Rarity.RareUltra to 6
+    )
+
+    override fun buildRound(cards: List<Card>): Round {
+
+        if (cards.isEmpty()) {
+            return Round()
+        }
+
+        var cardList = cards
+        var randomNum: Int = randomiser.getIntInRange(0, cardList.size - 1)
+        val cardOne: Card = cardList.get(randomNum)
+        var cardTwo: Card = cardOne
+        cardList = cardList.filterNot {compareTo(cardOne, it) == 0} // dropping all cards with rarity equal to cardOne
+
+        while (compareTo(cardOne, cardTwo) == 0 && cardList.isNotEmpty()) {
+            randomNum = randomiser.getIntInRange(0, cardList.size - 1)
+            cardTwo = cardList.get(randomNum)
+            cardList = cardList.filterNot {compareTo(cardTwo, it) == 0} // dropping all cards with rarity equal to cardTwo
+        }
+
+        if (compareTo(cardOne, cardTwo) == 0) {
+            return Round()
+        }
+
+        val listOfCards = listOf(cardOne, cardTwo)
+        val winnerCard: Card = getRarestCard(cardOne, cardTwo)
+
+        return Round(listOfCards, winnerCard)
+    }
+
+    override fun getRarestCard(cardOne: Card, cardTwo: Card): Card {
+
+        if(compareTo(cardOne, cardTwo) > 0) {
+            return cardOne
+        }
+        return cardTwo
+    }
+
+    override fun compareTo(cardOne: Card, cardTwo: Card): Int {
+
+        val cardOneRarity = mapRarities.getValue(cardOne.rarity)
+        val cardTwoRarity = mapRarities.getValue(cardTwo.rarity)
+
+        return cardOneRarity - cardTwoRarity
+    }
 }
